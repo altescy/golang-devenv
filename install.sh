83a58f805be281399b9e87c350899f0602e340f4
altescy <altescy@gmail.com>
2019-04-13 06:29:29 +0900

    modify install path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/.gitignore > ${pwd}/.gitignore
-curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/Dockerfile > ${pwd}/Dockerfile
-curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/Makefile > ${pwd}/Makefile
+curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/.gitignore > .gitignore
+curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/Dockerfile > Dockerfile
+curl -fsSL https://raw.githubusercontent.com/altescy/golang-devenv/master/Makefile > Makefile
